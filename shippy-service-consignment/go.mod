fa2510fa35e246027b8eb7c3b7f74315bc8e60a6
Quentin Desrame <quen2404@gmail.com>
2020-07-15 17:22:27 +0200

    feat(consignment): store data in mongodb database

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -5,7 +5,9 @@ go 1.14
 require (
 	github.com/golang/protobuf v1.4.2
 	github.com/micro/go-micro/v2 v2.9.1
+	github.com/pkg/errors v0.9.1
 	github.com/quen2404/shippy/shippy-service-vessel v0.0.0-20200715132542-28c3721c8433
+	go.mongodb.org/mongo-driver v1.3.5
 	google.golang.org/protobuf v1.23.0
 )
 
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -1,115 +1,55 @@
+// shippy-service-consignment/main.go
 package main
 
 import (
-	"log"
-	"sync"
-
-	// Import the generated protobuf code
 	"context"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/micro/go-micro/v2"
+	"github.com/quen2404/shippy/shippy-service-consignment/datastore"
+	"github.com/quen2404/shippy/shippy-service-consignment/handler"
 	pb "github.com/quen2404/shippy/shippy-service-consignment/proto/consignment"
+	"github.com/quen2404/shippy/shippy-service-consignment/repository"
 	vesselProto "github.com/quen2404/shippy/shippy-service-vessel/proto/vessel"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	mu           sync.RWMutex
-	consignments []*pb.Consignment
-}
-
-// Create a new consignment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	repo.mu.Unlock()
-	return consignment, nil
-}
-
-// GetAll consignments
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type consignmentService struct {
-	repo         repository
-	vesselClient vesselProto.VesselService
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *consignmentService) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	// Here we call a client instance of our vessel service with our consignment weight,
-	// and the amount of containers as the capacity value
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
-
-	// We set the VesselId as the vessel we got back from our
-	// vessel service
-	req.VesselId = vesselResponse.Vessel.Id
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *consignmentService) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
 func main() {
-
-	repo := &Repository{}
-
-	// Create a new service. Optionally include some options here.
+	// Set-up micro instance
 	service := micro.NewService(
-
-		// This name must match the package name given in your protobuf definition
 		micro.Name("shippy.service.consignment"),
 	)
 
-	vesselClient := vesselProto.NewVesselService("shippy.service.vessel", service.Client())
-
-	// Init will parse the command line flags.
 	service.Init()
 
-	// Register service
-	if err := pb.RegisterShippingServiceHandler(service.Server(), &consignmentService{repo, vesselClient}); err != nil {
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	client, err := datastore.CreateClient(context.Background(), uri, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	h := handler.NewHandler(
+		repository.NewMongoRepository(client.Database("shippy").Collection("consignments")),
+		vesselProto.NewVesselService("shippy.service.client", service.Client()),
+	)
+
+	// Register handlers
+	if err := pb.RegisterShippingServiceHandler(service.Server(), h); err != nil {
 		log.Panic(err)
 	}
 
 	// Run the server
 	if err := service.Run(); err != nil {
-		log.Panic(err)
+		fmt.Println(err)
 	}
 }
